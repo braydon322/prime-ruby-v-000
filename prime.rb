cfd43e460ca17ff6e2cf0102076fa28f1c9b46bf
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 08:37:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,45 +1,25 @@
-# Add  code here!
-# def prime?(integer)
-# i = 1
-#   if integer <= 1
-#     false
-#   elsif integer == 2
-#     true
-#   elsif integer > 2
-#     while i < integer
-#         if integer % i == 0
-#           false
-#         end
-#       i+= 1
-#     end
-#   end
-# end
-
 def prime?(number)
-odd_numbers = []
-is_it_odd = true
-  if (number % 2 == 0)
-    if number == 2
-      true
-    else
-      is_it_odd = false
-    end
-  else
-    is_it_odd = true
-  end
-
-  if is_it_odd = true
-    if number <= 1
-      false
-    elsif number >= 3
-      i = 1
-      while i < number
-        if number % i == 0
-          true
-        end
-        i += 1
-      end
-    end
-  else
-  end
+  if number <= 1
+    false
+  elsif number == 2
+    true
+  elsif number == 3
+    true
+  elsif number % 2 == 0
+    false
+  elsif number % 3 == 0
+    
+  
 end
+
+    # i = 5
+    # w = 2
+    # 
+    # while i * i <= n:
+    #     if n % i == 0:
+    #         return False
+    # 
+    #     i += w
+    #     w = 6 - w
+    # 
+    # return True

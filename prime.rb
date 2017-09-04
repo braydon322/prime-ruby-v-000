0392d0ed4431f28b7e1f8c0dc05d945d83369f55
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 08:15:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,28 @@
 # Add  code here!
-def prime?(integer)
-i = 1
-  if integer <= 1
-    false
-  elsif integer == 2
+# def prime?(integer)
+# i = 1
+#   if integer <= 1
+#     false
+#   elsif integer == 2
+#     true
+#   elsif integer > 2
+#     while i < integer
+#         if integer % i == 0
+#           false
+#         end
+#       i+= 1
+#     end
+#   end
+# end
+
+def prime?(number)
+
+  # PRIME NUMBERS - 2, 3, 5, 7, 9, 11, 13, 17, 23
+  while number > 2
     true
-  elsif integer > 2
-    while i < integer
-        if integer % i == 0
-          false
-        end
-      i+= 1
-    end
   end
-end
+  if number == 2
+    true
+  elsif number < 2
+    false
+  end

c90aabdad61d9ff3bd91b61f964407fb784bfb1f
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 08:46:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,7 @@
 def prime?(number)
   if number < 0
     false
-  elsif number == 2
-    true
-  elsif number == 3
-    true
-  elsif number % 2 == 0
-    false
-  elsif number % 3 == 0
-    i = 1
-      while i < number
-        if number % i == 0
-        false
-        end
-        i +=1
-      end
   end
-  true
-end
 
     # i = 5
     # w = 2

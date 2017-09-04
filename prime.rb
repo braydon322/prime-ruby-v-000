6a113641ee487e3e4c9b002687e465e8e76ce7a9
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 08:58:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,5 @@
 def prime?(number)
+  prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] # Up to 100
   if number <= 0
     false
   elsif number == 1
@@ -9,14 +10,20 @@ def prime?(number)
     if number % 2 == 0
       false
     else
-      i = 5
-      while i < (number + 1)/2
-        if number % i == 0
+      i = 4
+      while i < number
+        if (number == prime_numbers[i])
+          true
+        elsif (number % i == 0)
           false
         end
-        i+=1
+        i += 2
+
+
+
+
+
       end
-      true
     end
   end
 

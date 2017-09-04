79a1e351772d75ee491ec7494ab5dcdbdb3b3fee
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 09:16:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,4 @@
 
 def prime?(integer)
 factors_array = (2..(integer - 1)).to_a
-factors_array.any? { |factor|
-if integer % factor == 0
-  false
-elsif integer <= 0
-  false
-elsif integer == 1
-  false
-else
-  true
-end
-}
-end
-
-
 (factors_array.any? {|factor| integer % factor == 0} || integer <= 0 || integer == 1) ? false : true

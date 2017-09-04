34c183e3f879fb1aed54efd1187d4dde418c7b9d
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-04 07:56:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(integer)
+if integer <= 1
+  false
+elsif integer == 2
+  true
+elsif integer == 3
+  true
+else
+  while i < integer
+    is_it_prime = true
+    i = 5
+    while i < (integer/2)
+      
+    
+end
